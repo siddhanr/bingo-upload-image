e78fc966cf4fee80bc315e0fd70aaa5b2dbc19df
Siddha Rusdi <siddhanrusdi@gmail.com>
2019-03-06 11:13:49 +0700

    add prediction client

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
 import os
-from flask import Flask, flash, request, redirect, url_for, render_template
+from flask import Flask, flash, request, redirect, url_for, render_template,jsonify
 from werkzeug.utils import secure_filename
+from predict import get_prediction
 
-UPLOAD_FOLDER	= "test-upload/"
+UPLOAD_FOLDER	= "image-storage/"
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 ALLOWED_EXTENSIONS = set(['jpg'])
+project_id = "happyhackathon" 
+model_id = "ICN801453283093139314"
 
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
@@ -17,15 +20,18 @@ def upload_file():
 
 @app.route("/uploader", methods=["POST"])
 def upload_image():
+    req = request
+    filedata = request.files['file'].read()
     file = request.files['file']
     filename = secure_filename(file.filename)
     if file.filename == '':
             return redirect(request.url)
     if file and allowed_file(file.filename):
-        filename = secure_filename(file.filename)
-        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-        print(file)
-        return "file has been uploaded successfully"
+        res = get_prediction(filedata, project_id, model_id)
+        if res.payload[0].classification.score > 0.5:
+            response = {"keyword":res.payload[0].display_name}
+            return jsonify(response)
+
     else:
         return "file format has to be jpg"
 
---
+++ predict.py
@@ -0,0 +1,17 @@
+import sys
+
+from google.cloud import automl_v1beta1
+from google.cloud.automl_v1beta1.proto import service_pb2
+
+
+def get_prediction(content, project_id, model_id):
+  prediction_client = automl_v1beta1.PredictionServiceClient()
+
+  name = 'projects/{}/locations/us-central1/models/{}'.format(project_id, model_id)
+  payload = {'image': {'image_bytes': content }}
+  params = {}
+  request = prediction_client.predict(name, payload, params)
+  return request
+
+    
+    
